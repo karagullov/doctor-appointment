d6f970360e0f16c48b4bce7c7a6978517d8a8667
erlan <erlank123456789@gmail.com>
2025-05-17 01:16:18 +0600

    first commit

---
+++ server/middleware/cors.middleware.js
@@ -0,0 +1,11 @@
+function cors(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-HEADERS",
+    "Content-Type, Authorization, X-Requested-With"
+  );
+  next();
+}
+
+module.exports = cors;
